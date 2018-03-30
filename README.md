05aa1b7be3d6e6e131192de5a4a13ca7f2e9ee4d
Bednaross <rbednarek82@gmail.com>
2018-03-30 05:14:21 -0700

    Update script to use click

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AwsEC2Snap
-This project takes snapshots of EC2 instances 
+This project takes snapshots of EC2 instances.  It uses boto3 to connect to AWC account and performs snapshot of your VMs.
+
+##Configuring
+
+Configure your own AWS cli profile by running:
+`aws configure  --profile snap `
+
+##Running
+
+`pipenv run “python snap/snap.py”` 
--- snap/snap.py
+++ snap/snap.py
@@ -1,7 +1,22 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='Snap')
+ec2 = session.resource('ec2')
+
+
+@click.command()
+def get_instances_info():
+    "This function lists all EC2"
+    for i in ec2.instances.all():
+        print('; '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'])))
+
+    return
+
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='Snap')
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+    get_instances_info()
